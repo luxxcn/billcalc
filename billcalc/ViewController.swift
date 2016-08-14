8a0dac77d4386b6081389118d9c038baf07cbd66
xxing <953817@qq.com>
2016-08-14 11:44:44 +0800

    date time handler

--- billcalc/ViewController.swift
+++ billcalc/ViewController.swift
@@ -9,33 +9,6 @@
 import UIKit
 //import CoreData
 
-let D_DAY:Int = 24 * 3600
-let WEEK_CN:[String] = ["一", "二", "三", "四", "五", "六", "日"]
-
-extension NSDate {
-    func dayOfWeek()->Int{
-        let interval = self.timeIntervalSince1970;
-        let days = Int(interval) / D_DAY;
-        return Int((days - 3) % 7);
-    }
-    
-    func daysSinceToday()->Int{
-        var today = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        let todayStr = formatter.stringFromDate(today)
-        today = formatter.dateFromString(todayStr)! //抹掉时间，只保留日期，便于准确到期天数
-        //self = formatter.dateFromString(formatter.stringFromDate(self))
-        
-        return Int(self.timeIntervalSinceDate(today)) / D_DAY
-    }
-    
-    func format(format:String)->String{
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = format
-        return formatter.stringFromDate(self)
-    }
-}
 
 extension NSString {
     func isPureInt()->Bool{
